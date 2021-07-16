ed88fea54887a55cd4ac333e288f7a3c0ac221f2
Abhijit Bhutada <abhijit.bhutada@newvisionsoftpune.com>
2021-07-16 11:41:13 +0530

    initialise

--- www/fingerprintscan.js
+++ www/fingerprintscan.js
@@ -8,3 +8,7 @@ module.exports.scanfinger = function (arg0, success, error) {
     exec(success, error, 'fingerprintscan', 'scanfinger', [arg0]);
 
 }
+module.exports.initialise = function (arg0, success, error) {
+    exec(success, error, 'fingerprintscan', 'initialise', [arg0]);
+
+}
